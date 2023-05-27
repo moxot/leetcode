9ba1cf38f0a9c8b7db03cc06eb8287b4f5895ff5
Mykyta Menkov <nimenk@gmail.com>
2023-05-26 18:17:36 -0700

    change gitignore

--- rust_leetcode/target/debug/deps/rust-bd1c35ca977540e6.d
+++
@@ -1,7 +0,0 @@
-/home/nik/my-projects/leetcode/rust/target/debug/deps/rust-bd1c35ca977540e6: src/main.rs src/add_two_numbers.rs src/longest_substring_with_repeating_characters.rs
-
-/home/nik/my-projects/leetcode/rust/target/debug/deps/rust-bd1c35ca977540e6.d: src/main.rs src/add_two_numbers.rs src/longest_substring_with_repeating_characters.rs
-
-src/main.rs:
-src/add_two_numbers.rs:
-src/longest_substring_with_repeating_characters.rs:
--- rust_leetcode/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/home/nik/my-projects/leetcode/rust/target/debug/rust: /home/nik/my-projects/leetcode/rust/src/add_two_numbers.rs /home/nik/my-projects/leetcode/rust/src/longest_substring_with_repeating_characters.rs /home/nik/my-projects/leetcode/rust/src/main.rs
